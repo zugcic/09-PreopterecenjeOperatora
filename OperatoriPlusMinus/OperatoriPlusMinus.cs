bf59c26833c689781b3cd47463b36516f173b0ef
Stjepan Zugcic <stzugcic@vsite.hr>
2016-12-08 19:35:56 +0100

    Riješili smo Operatore PlusMinus

--- OperatoriPlusMinus/OperatoriPlusMinus.cs
+++ OperatoriPlusMinus/OperatoriPlusMinus.cs
@@ -35,17 +35,30 @@ namespace Vsite.CSharp
 
         public static KompleksniBroj operator +(KompleksniBroj br1, KompleksniBroj br2)
         {
-            // TODO: implementirati binarni operator + (za zbrajanje dva kompleksna broja) i provjeriti ispis pri izvođenju programa
-            throw new NotImplementedException();
+			// implementirati binarni operator + (za zbrajanje dva kompleksna broja) i provjeriti ispis pri izvođenju programa
+			return new KompleksniBroj(br1.RealniDio + br2.RealniDio, br1.ImaginarniDio + br2.ImaginarniDio);
+			throw new NotImplementedException();
         }
 
-        // TODO: Implementirati binarni operator - (za oduzimanje dva kompleksna broja)
-        
-        // TODO: Implementirati preopterećeni unarni operator - (za promjenu predznaka)
+		// Implementirati binarni operator - (za oduzimanje dva kompleksna broja)
+		public static KompleksniBroj operator -(KompleksniBroj br1, KompleksniBroj br2)
+		{
+		
+			return new KompleksniBroj(br1.RealniDio - br2.RealniDio, br1.ImaginarniDio - br2.ImaginarniDio);
+			throw new NotImplementedException();
+		}
+		// Implementirati preopterećeni unarni operator - (za promjenu predznaka)
+		public static KompleksniBroj operator -(KompleksniBroj br1)
+		{
 
-        // TODO: Otkomentirati naredbu koja ga poziva u Main te provjeriti ispis pri izvođenju programa
+			return new KompleksniBroj(-br1.RealniDio, -br1.ImaginarniDio);
+			throw new NotImplementedException();
+		}
 
-    }
+
+		// Otkomentirati naredbu koja ga poziva u Main te provjeriti ispis pri izvođenju programa
+
+	}
 
     class OperatoriPlusMinus
     {
@@ -58,11 +71,11 @@ namespace Vsite.CSharp
             Console.WriteLine("({0}) + ({1}) = {2}", kb1, kb2, zbroj);
 
             // Naredba koja poziva binarni operator -
-            //KompleksniBroj razlika = kb2 - kb1;
-            //Console.WriteLine("({0}) + ({1}) = {2}", kb1, kb2, razlika);
+            KompleksniBroj razlika = kb2 - kb1;
+            Console.WriteLine("({0}) - ({1}) = {2}", kb2, kb1, razlika);
 
             // Naredba koja poziva unarni operator -
-            //Console.WriteLine("-[({0}) + ({1})] = {2}", kb1, kb2, -(zbroj));
+            Console.WriteLine("-[({0}) + ({1})] = {2}", kb1, kb2, -(zbroj));
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
